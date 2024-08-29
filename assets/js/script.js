aea3717f7218931ee8ec8a640cef342ca0b6e6ef
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-08-29 14:42:57 +0200

    Organized the to-do list for the script.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// 1) Creare le 90 celle della tombola
+
+// 2) Creare un bottone per l' estrazione di un numero random da 1 a 90
+
+// 3) Evidenziare la cella corrispondente al numero estratto
+
+// 4) Mantenere evidenzate le celle precedentemente estratte.
+
+// 5-EXTRA) Un numero estratto non può essere estratto di nuovo
+
+// 6-EXTRA) Creare una tabella da 24 celle con numeri random, che si comporterà come il tabellone principale
+
+// 7-EXTRA) Permettere all' utente di selezionare quante tabelline avere e generarle a schermo all' inizio della partita
