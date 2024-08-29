c81372f9fa3bbc64ba571f850b0311a083dd4c0e
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-08-29 16:11:38 +0200

    done the "tombola"

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@
 
 const boardContainer = document.querySelector(".board-container");
 const board = document.getElementById("board");
+let extractedNumber = 0;
 
 // 1) Creare le 90 celle della tombola ed il bottone per estrarre i numeri
 const createBoard = () => {
   for (let i = 1; i <= 90; i++) {
     const div = document.createElement("div");
     div.className = "number";
+    div.id = i;
     const h3 = document.createElement("h3");
     h3.innerText = i;
     div.appendChild(h3);
@@ -17,6 +19,25 @@ const createBoard = () => {
   extractButton.id = "extract-number";
   extractButton.innerText = "Extract";
   boardContainer.appendChild(extractButton);
+
+  //2)
+  extractButton.onclick = () => {
+    extractedNumber = Math.ceil(Math.random() * 90);
+
+    //3)
+    const selectCell = (number) => {
+      const id = number.toString();
+
+      const cell = document.getElementById(id);
+      cell.classList.add("extracted");
+    };
+    selectCell(extractedNumber);
+    console.log(extractedNumber);
+  };
+};
+
+window.onload = () => {
+  createBoard();
 };
 
 // 2) Creare la funzione per l' estrazione di un numero random da 1 a 90 per il bottone
@@ -30,7 +51,3 @@ const createBoard = () => {
 // 6-EXTRA) Creare una tabella da 24 celle con numeri random, che si comporterà come il tabellone principale
 
 // 7-EXTRA) Permettere all' utente di selezionare quante tabelline avere e generarle a schermo all' inizio della partita
-
-window.onload = () => {
-  createBoard();
-};
